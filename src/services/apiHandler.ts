b1fa169c923250d9de50f15516cfef00277a86a7
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2025-03-16 13:55:29 +0530

    create api handler

---
+++ src/services/apiHandler.ts
@@ -0,0 +1,78 @@
+import axios, { AxiosHeaders, AxiosRequestConfig, AxiosResponse } from "axios";
+import * as apiUrls from "./apiUrls.ts";
+import {ApiObject, ApiResponse} from "../interfaces/api.ts";
+import $ from 'jquery'
+
+export const callApi = async (apiObject: ApiObject): Promise<ApiResponse> => {
+
+    if (apiObject.loading === undefined) $(".loadingEffect").css("display", "block");
+
+    let result: ApiResponse;
+    const method = apiObject.method?.toLowerCase() ?? "get";
+
+    const headers = new AxiosHeaders({
+        "Content-Type": apiObject.urlEncoded
+            ? "application/x-www-form-urlencoded"
+            : apiObject.multipart
+                ? "multipart/form-data"
+                : "application/json",
+    });
+
+    // If authentication is required, add token
+    if (apiObject.authentication) {
+        const accessToken = localStorage.getItem("accessToken");
+        if (accessToken) {
+            headers.set("Authorization", `Bearer ${accessToken}`);
+        }
+    }
+
+    const url = `${import.meta.env.VITE_SERVER_URL}/${import.meta.env.VITE_BASE_PATH}/${apiObject.endpoint}`;
+
+    try {
+        const config: AxiosRequestConfig = {
+            method,
+            url,
+            headers,
+            data: method !== "get" && method !== "delete" ? apiObject.body : undefined,
+        };
+
+        const response: AxiosResponse<ApiResponse> = await axios(config);
+
+        $(".loadingEffect").css("display", "none");
+        result = response.data;
+
+    } catch (error) {
+        $(".loadingEffect").css("display", "none");
+
+        if (axios.isAxiosError(error) && error.response) {
+            result = {success: false, statusCode: error.response.data?.statusCode,  message: error.response.data?.message};
+        } else {
+            result = {success: false, message: "Your connection was interrupted" };
+        }
+
+        // if (axios.isAxiosError(error) && error.response) {
+        //     if (error.response.status === 705 || error.response.status === 605) {
+        //         if (apiObject.state === "login") {
+        //             return { success: false, message: error.response.data?.message || "Login error" };
+        //         }
+        //     } else if (error.response.status === 401) {
+        //         if (apiObject.state === "login") {
+        //             return { success: false, message: "Invalid login details, please try again!" };
+        //         }
+        //         if (apiObject.state === "renewToken") {
+        //             return { success: false, message: "Your session has expired. Please sign in again ..." };
+        //         }
+        //     } else {
+        //         result = { success: false, message: error.response.data?.message || "An error occurred" };
+        //     }
+        // } else {
+        //     result = { success: false, message: "Your connection was interrupted" };
+        // }
+        //
+        // if (!result.success && apiObject.toast === undefined) {
+        //     showToast({ type: "error", message: result.message || "An error occurred" });
+        // }
+    }
+
+    return result;
+};
---
+++ src/services/apiServices.ts
@@ -0,0 +1,121 @@
+import * as ApiService from "./apiHandler.ts"
+import {ApiObject, SignInObject, SignUpObject} from "../interfaces/api.ts";
+import {Address, Order} from "../interfaces/user.ts";
+
+export const signupService = async (obj: SignUpObject) => {
+    const apiObject: ApiObject = {}
+    apiObject.method = "POST"
+    apiObject.authentication = false
+    apiObject.endpoint = `users/signup`
+    apiObject.body = obj
+    return await ApiService.callApi(apiObject);
+}
+
+export const signinService = async (obj: SignInObject) => {
+    const apiObject: ApiObject = {}
+    apiObject.method = "POST"
+    apiObject.authentication = false
+    apiObject.endpoint = `users/signIn`
+    apiObject.body = obj
+    return await ApiService.callApi(apiObject);
+}
+
+export const userDetailsService = async () => {
+    const apiObject: ApiObject = {}
+    apiObject.method = "GET"
+    apiObject.authentication = true
+    apiObject.endpoint = `users/userDetails`
+    return await ApiService.callApi(apiObject);
+}
+
+export const productsService = async (currentPage: number) => {
+    const apiObject: ApiObject = {}
+    apiObject.method = "GET"
+    apiObject.authentication = false
+    apiObject.endpoint = `products?page=${currentPage}`
+    return await ApiService.callApi(apiObject);
+}
+
+export const productDetailsService = async (productId: string | undefined) => {
+    const apiObject: ApiObject = {}
+    apiObject.method = "GET"
+    apiObject.authentication = false
+    apiObject.endpoint = `products/${productId}`
+    return await ApiService.callApi(apiObject);
+}
+
+export const trendingService = async () => {
+    const apiObject: ApiObject = {}
+    apiObject.method = "GET"
+    apiObject.authentication = false
+    apiObject.endpoint = `products/trending`
+    return await ApiService.callApi(apiObject);
+}
+
+export const addCartItemService = async (productId: string) => {
+    const apiObject: ApiObject = {}
+    apiObject.method = "PATCH"
+    apiObject.authentication = true
+    apiObject.endpoint = `users/cart/${productId}`
+    return await ApiService.callApi(apiObject);
+}
+
+export const deleteCartItemService = async (productId: string) => {
+    const apiObject: ApiObject = {}
+    apiObject.method = "DELETE"
+    apiObject.authentication = true
+    apiObject.endpoint = `users/cart/${productId}`
+    return await ApiService.callApi(apiObject);
+}
+
+export const addWishlistItemService = async (productId: string) => {
+    const apiObject: ApiObject = {}
+    apiObject.method = "PATCH"
+    apiObject.authentication = true
+    apiObject.endpoint = `users/wishlist/${productId}`
+    return await ApiService.callApi(apiObject);
+}
+
+export const deleteWishlistItemService = async (productId: string) => {
+    const apiObject: ApiObject = {}
+    apiObject.method = "DELETE"
+    apiObject.authentication = true
+    apiObject.endpoint = `users/wishlist/${productId}`
+    return await ApiService.callApi(apiObject);
+}
+
+export const getUserAddressesService = async () => {
+    const apiObject: ApiObject = {}
+    apiObject.method = "GET"
+    apiObject.authentication = true
+    apiObject.endpoint = `users/addresses`
+    return await ApiService.callApi(apiObject);
+}
+
+export const saveNewUserAddressService = async (obj: Address) => {
+    const apiObject: ApiObject = {}
+    apiObject.method = "POST"
+    apiObject.authentication = true
+    apiObject.endpoint = `users/addresses`
+    apiObject.body = obj
+    return await ApiService.callApi(apiObject);
+}
+
+export const initializeNewOrderService = async (obj: Order) => {
+    const apiObject: ApiObject = {}
+    apiObject.method = "POST"
+    apiObject.authentication = true
+    apiObject.endpoint = `orders/init`
+    apiObject.body = obj
+    return await ApiService.callApi(apiObject);
+}
+
+export const completeOrderPaymentService = async (obj: Order) => {
+    const apiObject: ApiObject = {}
+    apiObject.method = "PATCH"
+    apiObject.authentication = true
+    apiObject.endpoint = `orders/complete`
+    apiObject.body = obj
+    return await ApiService.callApi(apiObject);
+}
+
